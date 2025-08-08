bfccbe57bbe05ca0e51dfec0beb7a88f6dd2a8e1
Throughcode <Throughcode@users.noreply.github.com>
2025-08-07 23:07:09 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AwwwardsHero from "./AwwwardsHero.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AwwwardsHero />
+  </React.StrictMode>
+);
